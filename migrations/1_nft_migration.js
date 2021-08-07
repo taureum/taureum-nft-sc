111c7c99ae002fd873fa95d25907667318bd10ab
thanhn-inc <thanh.n@incognito.org>
2021-08-07 16:23:25 +0100

    Update migration script

--- migrations/1_nft_migration.js
+++ migrations/1_nft_migration.js
@@ -34,19 +34,19 @@ module.exports = async function (deployer, network) {
         await deployer.deploy(TaureumNFT);
         mainContract = await TaureumNFT.deployed();
         mainContractAddress = await mainContract.address
-        console.log("Main contract deployed at address", mainContractAddress)
+        console.log("Main contract (ERC721) deployed at address", mainContractAddress)
         setConfig('deployed.' + network + '.TaureumERC721', mainContractAddress)
 
         await deployer.deploy(TaureumNFTEnum);
         mainContract = await TaureumNFTEnum.deployed();
         mainContractAddress = await mainContract.address
-        console.log("Main contract (enumerable) deployed at address", mainContractAddress)
+        console.log("Main contract (ERC721Enumerable) deployed at address", mainContractAddress)
         setConfig('deployed.' + network + '.TaureumERC721Enumerable', mainContractAddress)
 
         await deployer.deploy(TaureumNFTLazyMint);
         mainContract = await TaureumNFTLazyMint.deployed();
         mainContractAddress = await mainContract.address
-        console.log("Main contract (lazyMint) deployed at address", mainContractAddress)
+        console.log("Main contract (ERC721LazyMint) deployed at address", mainContractAddress)
         setConfig('deployed.' + network + '.TaureumERC721LazyMint', mainContractAddress)
     }
 };
--- migrations/2_erc1155_migration.js
+++ migrations/2_erc1155_migration.js
@@ -1,14 +1,31 @@
 const { setConfig } = require('./config.js')
 
-const TaureumNFT = artifacts.require("TaureumERC1155");
+const TaureumERC1155 = artifacts.require("TaureumERC1155");
+const TaureumERC1155LazyMint = artifacts.require("TaureumERC1155LazyMint");
 
 module.exports = async function (deployer, network) {
-    let mainContract
-    let mainContractAddress
-    await deployer.deploy(TaureumNFT, "");
-    mainContract = await TaureumNFT.deployed();
-    mainContractAddress = await mainContract.address
-    console.log("Main contract (ERC1155) deployed at address", mainContractAddress)
-    setConfig('deployed.' + network + '.TaureumERC1155', mainContractAddress)
+    if (network !== "development") {
+        let mainContract
+        let mainContractAddress
 
+        await deployer.deploy(TaureumERC1155LazyMint, "");
+        mainContract = await TaureumERC1155LazyMint.deployed();
+        mainContractAddress = await mainContract.address
+        console.log("Main contract (ERC1155LazyMint) deployed at address", mainContractAddress)
+        setConfig('deployed.' + network + '.TaureumERC1155LazyMint', mainContractAddress)
+    } else {
+        let mainContract
+        let mainContractAddress
+        await deployer.deploy(TaureumERC1155, "");
+        mainContract = await TaureumERC1155.deployed();
+        mainContractAddress = await mainContract.address
+        console.log("Main contract (ERC1155) deployed at address", mainContractAddress)
+        setConfig('deployed.' + network + '.TaureumERC1155', mainContractAddress)
+
+        await deployer.deploy(TaureumERC1155LazyMint, "");
+        mainContract = await TaureumERC1155LazyMint.deployed();
+        mainContractAddress = await mainContract.address
+        console.log("Main contract (ERC1155LazyMint) deployed at address", mainContractAddress)
+        setConfig('deployed.' + network + '.TaureumERC1155LazyMint', mainContractAddress)
+    }
 };
