cfdda1de80bad703f7072a1ff684df6618b6be89
thanhn-inc <thanh.n@incognito.org>
2021-08-24 08:32:13 +0100

    Update flatten script

---
+++ scripts/flatten.sh
@@ -0,0 +1,5 @@
+../node_modules/.bin/truffle-flattener ../contracts/tokens/ERC721/TaureumERC721.sol > ../contracts/flattened/TaureumERC721.sol
+../node_modules/.bin/truffle-flattener ../contracts/tokens/ERC721/TaureumERC721Enumerable.sol > ../contracts/flattened/TaureumERC721Enumerable.sol
+../node_modules/.bin/truffle-flattener ../contracts/tokens/ERC721/TaureumERC721LazyMint.sol > ../contracts/flattened/TaureumERC721LazyMint.sol
+../node_modules/.bin/truffle-flattener ../contracts/tokens/ERC1155/TaureumERC1155.sol > ../contracts/flattened/TaureumERC1155.sol
+../node_modules/.bin/truffle-flattener ../contracts/tokens/ERC1155/TaureumERC1155LazyMint.sol > ../contracts/flattened/TaureumERC1155LazyMint.sol
