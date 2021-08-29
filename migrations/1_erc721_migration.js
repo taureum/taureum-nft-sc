5fc0918fc66f587374bc768638fc91a6ee7ec186
thanhn-inc <thanh.n@incognito.org>
2021-08-29 04:52:45 +0100

    Update truffle config

---
+++ migrations/1_erc721_migration.js
@@ -0,0 +1,40 @@
+const { setConfig } = require('./config.js')
+
+const TaureumNFT = artifacts.require("TaureumERC721");
+const TaureumNFTEnum = artifacts.require("TaureumERC721Enumerable");
+const TaureumNFTLazyMint = artifacts.require("TaureumERC721LazyMint");
+
+module.exports = async function (deployer, network) {
+    if (network === 'mainnet') {
+        let mainContract
+        let mainContractAddress
+
+        await deployer.deploy(TaureumNFTLazyMint);
+        mainContract = await TaureumNFTLazyMint.deployed();
+        mainContractAddress = await mainContract.address
+        console.log("Main contract (lazyMint) deployed at address", mainContractAddress)
+        setConfig('deployed.' + network + '.TaureumERC721LazyMint', mainContractAddress)
+
+    } else {
+        let mainContract
+        let mainContractAddress
+
+        // await deployer.deploy(TaureumNFT);
+        // mainContract = await TaureumNFT.deployed();
+        // mainContractAddress = await mainContract.address
+        // console.log("Main contract (ERC721) deployed at address", mainContractAddress)
+        // setConfig('deployed.' + network + '.TaureumERC721', mainContractAddress)
+        //
+        // await deployer.deploy(TaureumNFTEnum);
+        // mainContract = await TaureumNFTEnum.deployed();
+        // mainContractAddress = await mainContract.address
+        // console.log("Main contract (ERC721Enumerable) deployed at address", mainContractAddress)
+        // setConfig('deployed.' + network + '.TaureumERC721Enumerable', mainContractAddress)
+
+        await deployer.deploy(TaureumNFTLazyMint);
+        mainContract = await TaureumNFTLazyMint.deployed();
+        mainContractAddress = await mainContract.address
+        console.log("Main contract (ERC721LazyMint) deployed at address", mainContractAddress)
+        setConfig('deployed.' + network + '.TaureumERC721LazyMint', mainContractAddress)
+    }
+};
